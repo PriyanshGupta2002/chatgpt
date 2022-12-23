54a36bb234b8ea4ebc2d1be570419b9b8d2aba48
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2022-12-24 00:58:19 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handleSubmit = async(e)=>{
     chatContainer.scrollTop=chatContainer.scrollHeight
     const messageDiv = document.getElementById(uniqueId)
     loader(messageDiv)
-    const response=await fetch('http://localhost:3000',{
+    const response=await fetch('https://codehelper.onrender.com/',{
         method:"POST",
         headers:{
             'Content-Type':"application/json"
